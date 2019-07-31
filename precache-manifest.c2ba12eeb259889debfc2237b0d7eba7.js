d3923a218add29a9290c8284146a48a6745107df
DeMT <gn00468461@gmail.com>
2019-08-01 00:27:00 +0800

    Updates

---
+++ precache-manifest.c2ba12eeb259889debfc2237b0d7eba7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58402b610087c7b18890ee9d2e488f12",
+    "url": "/Monster-Rolodex/index.html"
+  },
+  {
+    "revision": "2cd33c5999116401de60",
+    "url": "/Monster-Rolodex/static/css/main.99b165af.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/Monster-Rolodex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "2cd33c5999116401de60",
+    "url": "/Monster-Rolodex/static/js/main.959f66fc.chunk.js"
+  },
+  {
+    "revision": "1c53a097523ea440329e",
+    "url": "/Monster-Rolodex/static/js/runtime~main.5e73536d.js"
+  }
+]);
